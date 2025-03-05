2207907ba9372d074157d6d2d28d4b88350d040a
Abdelillah07 <abdelbess110@gmail.com>
2025-03-05 14:47:05 +0100

    Abdelillah

--- fenetre_entre.py
+++ fenetre_entre.py
@@ -1,15 +1,45 @@
 import tkinter as tk
 
-CANVAS_WIDTH, CANVAS_HEIGHT = 250, 250
+def access():
+    lignes = entre_lignes.get()
+    colonnes = entre_colonnes.get()
+    titre.destroy()
+    label_lignes.destroy()
+    label_colonnes.destroy()
+    entre_lignes.destroy()
+    entre_colonnes.destroy()
+    button_confirmer.destroy()
+
+def default():
+    lignes = 6
+    colonnes = 7
+    titre.destroy()
+    label_lignes.destroy()
+    label_colonnes.destroy()
+    entre_lignes.destroy()
+    entre_colonnes.destroy()
+    button_confirmer.destroy()
+    button_default.destroy()
+
+lignes, colonnes = 0, 0
 
 fenetre = tk.Tk()
-canvas = tk.Canvas(fenetre, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+fenetre.title("Puissance 4")
 
 titre = tk.Label(fenetre, text="Choisis le nombre de:")
-lignes = tk.Label(fenetre, text="Lignes")
-colonnes = tk.Label(fenetre, text="Colonnes")
+label_lignes = tk.Label(fenetre, text="Lignes")
+label_colonnes = tk.Label(fenetre, text="Colonnes")
+entre_lignes = tk.Entry(fenetre)
+entre_colonnes = tk.Entry(fenetre)
+button_confirmer = tk.Button(fenetre, text="Confirmer", command=access)
+button_default = tk.Button(fenetre, text="Par défaut", command=default)
 
-
-titre.grid(row=0, column=0)
+titre.grid(row=0, column=0, columnspan=2)
+label_lignes.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+label_colonnes.grid(row=2, column=0, padx=10, pady=10, sticky="e")
+entre_lignes.grid(row=1, column=1)
+entre_colonnes.grid(row=2, column=1)
+button_default.grid(row=3, column=0)
+button_confirmer.grid(row=3, column=1)
 
 fenetre.mainloop()
