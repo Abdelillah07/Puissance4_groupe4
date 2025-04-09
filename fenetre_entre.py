fbccb36c123a32ae7a6c512248389a0353059859
Abdelillah07 <abdelbess110@gmail.com>
2025-04-09 19:43:07 +0200

    Abdelillah

--- fenetre_entre.py
+++ fenetre_entre.py
@@ -1,16 +1,14 @@
 import tkinter as tk
-import main
-
-def ouvrir_fenetre():
-    main.fen_accueil()
+from main import fen_parties
+from set_et_match import fen_parties_set_et_match
 
 fen = tk.Tk()
 fen.title("Puissance 4")
 
-label_haut  = tk.Label(fen, text="Mode de Jeu")
-button_1vs1 = tk.Button(fen, text="1 VS 1", command=ouvrir_fenetre)
-button_1vsRobot = tk.Button(fen, text="1 VS ROBOT", command=ouvrir_fenetre)
-button_Set_et_Match = tk.Button(fen, text="Set et Match", command=ouvrir_fenetre)
+label_haut  = tk.Label(fen, text="Mode de jeu")
+button_1vs1 = tk.Button(fen, text="1 VS 1", command=fen_parties)
+button_1vsRobot = tk.Button(fen, text="1 VS ROBOT")
+button_Set_et_Match = tk.Button(fen, text="Set et Match", command=fen_parties_set_et_match)
 
 label_haut.pack(padx=10, pady=10)
 button_1vs1.pack(padx=10, pady=10)
